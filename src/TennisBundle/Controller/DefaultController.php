d999a110fccbc0a0fe6b8c1d69486ce579cb5eaa
KyoLuma <couchotcynthia99@gmail.com>
2017-05-16 14:34:02 +0200

    hjg commit

---
+++ src/TennisBundle/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace TennisBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="kjj")
+     */
+    public function indexAction()
+    {
+        return $this->render('TennisBundle:Default:index.html.twig');
+    }
+    /**
+     * @Route("/gprofil", name="gprofil")
+     */
+    public function gprofilAction()
+    {
+        return $this->render('TennisBundle:Default:general_profil.html.twig');
+    }
+    /**
+     * @Route("/matches", name="matches")
+     */
+    public function matchesAction()
+    {
+        return $this->render('TennisBundle:Default:matches.html.twig');
+    }
+}
---
+++ src/TennisBundle/TennisBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TennisBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TennisBundle extends Bundle
+{
+}
